235621c4bca86bb311a482f465faa3fbc0ccd65e
kaiceniza <kaiceniza@users.noreply.github.com>
2021-04-22 21:51:13 +0800

    Revert "initial commit"
    This reverts commit 0491f4931e1ffb3c8de37a1bf3e873ad162289de.

--- controllers/accountController.js
+++
@@ -1,5 +0,0 @@
-const account = require("../models/account");
-
-exports.createAccount = async (req, res) => {
-    console.log("Create an account");
-}
--- routes/accountRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const accountController = require("../controllers/accountController");
-
-router.get("/testing", accountController.createAccount);
-
-module.exports = router;
